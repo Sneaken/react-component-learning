358181f05c98cd901c23cb15f54a0f8a14b2ef46
Sneaken <924393527@qq.com>
2023-01-07 17:59:58 +0800

    test: 测试内容

--- src/pages/Tabs.tsx
+++ src/pages/Tabs.tsx
@@ -1,27 +1,28 @@
 import React, { forwardRef, Ref, useState } from 'react';
 import { Tabs as AntdTab } from 'antd';
-import { Tabs } from '../components';
+import { Tabs, type Tab } from '../components';
 
 const Label = forwardRef((props, ref: Ref<any>) => {
   return <p ref={ref}>222</p>;
 });
 
-const items = [
+const items: Tab[] = [
   {
-    label: <Label />,
-    key: '1',
-    className: '222',
     children: `Content of Tab Pane 1`,
+    className: '222',
+    key: '1',
+    label: <Label />,
   },
   {
-    label: `Tab 2`,
-    key: '2',
     children: `Content of Tab Pane 2`,
+    disabled: false,
+    key: '2',
+    label: `Tab 2`,
   },
   {
-    label: `Tab 3`,
-    key: '3',
     children: `Content of Tab Pane 3`,
+    key: '3',
+    label: `Tab 3`,
   },
 ];
 const PageOfTabs = () => {
@@ -30,14 +31,16 @@ const PageOfTabs = () => {
     setActiveKey(key);
   };
 
+  const size = 'large';
+
   return (
     <>
       <div style={{ padding: '0 20px' }}>
         <div style={{ float: 'left', width: '40vw' }}>
-          <Tabs activeKey={activeKey} onChange={onChange} items={items} />
+          <Tabs size={size} activeKey={activeKey} onChange={onChange} items={items} />
         </div>
         <div style={{ float: 'left', width: '40vw' }}>
-          <AntdTab activeKey={activeKey} onChange={onChange} items={items} />
+          <AntdTab size={size} activeKey={activeKey} onChange={onChange} items={items} />
         </div>
       </div>
     </>
