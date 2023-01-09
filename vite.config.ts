5ccc666d64d5c70a0cdea7cabbd0e9b3c7430c9c
Sneaken <924393527@qq.com>
2023-01-09 21:51:31 +0800

    chore: vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,18 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
   },
+  resolve: {
+    alias: [
+      {
+        find: '@/',
+        replacement: resolve(__dirname, 'src/'),
+      },
+      {
+        find: '@tests',
+        replacement: resolve(__dirname, 'tests'),
+      },
+    ],
+  },
+
   plugins: [react()],
 });
